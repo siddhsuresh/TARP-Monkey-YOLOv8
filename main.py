56e2e0aa65e6f1f9f5c6a54b5f1f34bff943009f
Siddharth Suresh <siddh.suresh@gmail.com>
2023-04-12 11:35:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
         print('valid')
         if st.button('Detect'):
             with rd.stderr(format='markdown', to=st.sidebar), st.spinner('Wait for it...'):
-                print(subprocess.run(['yolo', 'task=detect', 'mode=predict', 'model=yolov8n.pt', 'conf=0.25', 'source={}'.format(source)],capture_output=True, universal_newlines=True).stderr)
+                print(subprocess.run(['yolo', 'task=detect', 'mode=predict', 'model=best (1).pt', 'conf=0.25', 'source={}'.format(source)],capture_output=True, universal_newlines=True).stderr)
 
                     
             if source_index == 0:
